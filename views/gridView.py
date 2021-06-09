6aad2904bec18f9676e3b06cbaccd4cb09cd2323
Cyril Berche <berchecyril@gmail.com>
2021-06-09 16:28:37 +0200

    Fenetre Help intégrée à gridView

--- views/gridView.py
+++ views/gridView.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QMainWindow, QMenu, QAction, QLabel, QPushButton, QG
 from PyQt5.QtCore import Qt
 from controllers.crtlMouvement import CrtlMouvement
 from model.grid import Grid
+from views.helpView import HelpView
 
 
 class GridView(QMainWindow):
@@ -124,8 +125,10 @@ class GridView(QMainWindow):
     def helpView(self):
         dialog = QDialog()
         dialog.setAttribute(Qt.WA_DeleteOnClose)
-        dialog.setWindowTitle("Help")
+        dialog.setWindowTitle("Aide")
+        dialog.setFixedSize(500, 500)
 
-        label = QLabel("<h1> J'ai besoin d'aide </h1>", parent=dialog)
-        label.show()
+        QLabel("<h1>Aide du jeu !</h1><p>Le but du jeu est de combler les trous en y plaçant les différentes pierres.</p><p>Vous pouvez vous déplacer en utilisant :</p><p> fléche haut ( ou 'z' ) pour aller vers le haut;</p><p> fléche bas ( ou 's' ) pour aller vers le bas;</p><p> fléche gauche ( ou 'q' ) pour aller vers la gauche;</p><p> fléche droit ( ou 'd' ) pour aller vers la droite;</p>", parent=dialog)
+
         dialog.exec_()
+
